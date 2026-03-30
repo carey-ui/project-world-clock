1ca6b7cb1f89c0f77bd43a13afbe8f4d532fec71
carey-ui <careyf30@gmail.com>
2026-03-30 22:08:21 +0100

    add and intergrated Moments into Javascript for realtime information

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function updateTime() {
+  let cityOneElement = document.querySelector("#city-one");
+  let cityOneDateElement = cityOneElement.querySelector(".date");
+  let cityOneTimeElement = cityOneElement.querySelector(".time");
+  let cityOneTime = moment().tz("America/Los_Angeles");
+  cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do YYYY");
+  cityOneTimeElement.innerHTML = `${cityOneTime.format("h:mm:ss [<small>]A[</small>]")}`;
 
+  let cityTwoElement = document.querySelector("#city-two");
+  let cityTwoDateElement = cityTwoElement.querySelector(".date");
+  let cityTwoTimeElement = cityTwoElement.querySelector(".time");
+  let cityTwoTime = moment().tz("Australia/Sydney");
+  cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do YYYY");
+  cityTwoTimeElement.innerHTML = `${cityTwoTime.format("h:mm:ss [<small>]A[</small>]")}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
